8fbc8eec25a6b818099ccf37dc3d129115d1f9ab
Khasi Marc <kmj426@nyu.edu>
2019-12-04 13:17:04 -0500

    Updating Typo

--- feature_selection.py
+++ feature_selection.py
@@ -14,14 +14,17 @@ def selectFeatures(merged_df_no_id, target, n_features_to_select, sel):
         merged_df_filtered =  fRegression(x, y, n_features_to_select)
     elif sel == MUTUAL_REGRESSION:
         merged_df_filtered = mutualInfoRegression(x, y, n_features_to_select)
+        
     return merged_df_filtered
     
 def fRegression(x, y, n_features_to_select):
     selector = SelectKBest(f_regression, n_features_to_select)
+    
     return postSelectKBest(x, y, selector)
 
 def mutualInfoRegression(x, y, n_features_to_select):
     selector = SelectKBest(mutual_info_regression, n_features_to_select)
+    
     return postSelectKBest(x, y, selector)
 
 def postSelectKBest(x, y, selector):
@@ -30,5 +33,5 @@ def postSelectKBest(x, y, selector):
     cols = selector.get_support(indices=True)
     # Create new dataframe with only desired columns, or overwrite existing
     features_df_new = x.iloc[:,cols]
-    features_df_new.columns
+    
     return features_df_new
