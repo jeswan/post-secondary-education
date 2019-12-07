b78ff7755c82ef87a8d7d34ea172dc91d2a7c60a
Jesse Swanson <js11133@nyu.edu>
2019-12-06 19:09:55 -0500

    added ref rf to feature selection

--- feature_selection.py
+++ feature_selection.py
@@ -4,19 +4,28 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_regression
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.feature_selection import RFE 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import RFECV
+import modeling_2 as model
+from sklearn.model_selection import StratifiedKFold
 
 F_REGRESSION = 0
 MUTUAL_REGRESSION = 1
+SFM_LASSO = 2
+RFE_RF = 3
+RFE_RF_OPTIMAL = 4
 
 def selectFeatures(merged_df_no_id, target, n_features_to_select, sel):
+    feature_cols = []
     x = merged_df_no_id.drop([target], axis=1)
     y = merged_df_no_id[target]
-    
+
     if sel == F_REGRESSION:
         feature_cols =  fRegression(x, y, n_features_to_select)
     elif sel == MUTUAL_REGRESSION:
         feature_cols = mutualInfoRegression(x, y, n_features_to_select)
-    
+    elif sel == RFE_RF:
+        feature_cols = recursiveFeaturesSelectRandomForest(x, y, n_features_to_select)
 
     # Create new dataframe with only desired columns
     new_df = merged_df_no_id.iloc[:,feature_cols]
@@ -29,6 +38,15 @@ def selectFeatures(merged_df_no_id, target, n_features_to_select, sel):
         new_df['Year'] = merged_df_no_id['Year']
         
     return new_df
+
+def recursiveFeaturesSelectRandomForest(x, y, n_features_to_select):
+    estimator = RandomForestRegressor()
+    rfe = RFE(estimator, n_features_to_select)
+    rfe.fit(x,y)
+
+    feature_cols = rfe.get_support(indices=True)
+    
+    return feature_cols
     
 def fRegression(x, y, n_features_to_select):
     selector = SelectKBest(f_regression, n_features_to_select)
@@ -44,5 +62,4 @@ def postSelectKBest(x, y, selector):
     selector.fit(x, y)
     # Get columns to keep
     cols = selector.get_support(indices=True)
-   
     return cols
